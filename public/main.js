9de61ca27c96a2847a943810f4c1b09a92deaea4
Benjamin <128577990+bencoronard@users.noreply.github.com>
2024-05-03 22:03:23 +0700

    Implemented DropZone class

--- public/main.js
+++ public/main.js
@@ -1,38 +1,33 @@
-import { PreviewList } from "./modules/PreviewList.mjs";
-import { ProgressBar } from "./modules/ProgressBar.mjs";
-import { StatusText } from "./modules/StatusText.mjs";
+import { DropZone } from './modules/DropZone.mjs';
+import { PreviewList } from './modules/PreviewList.mjs';
+import { ProgressBar } from './modules/ProgressBar.mjs';
+import { StatusText } from './modules/StatusText.mjs';
 
-const form = document.querySelector("form");
-const fileInput = document.getElementById("fileInput");
+const form = document.querySelector('form');
+const fileInput = document.getElementById('fileInput');
 
-const submitButton = document.getElementById("submitButton");
+const submitButton = document.getElementById('submitButton');
 
-const fileCounter = document.getElementById("fileCounter");
+const fileDropZone = new DropZone(document.getElementById('dropZone'));
+const fileUploads = new PreviewList(document.getElementById('fileUploads'));
+const filePreviews = new PreviewList(document.getElementById('filePreviews'));
+const statusMessage = new StatusText(document.getElementById('statusMessage'));
+const progressBar = new ProgressBar(document.querySelector('progress'));
 
-const fileUploads = new PreviewList(document.getElementById("fileUploads"));
-const filePreviews = new PreviewList(document.getElementById("filePreviews"));
-const statusMessage = new StatusText(document.getElementById("statusMessage"));
-const progressBar = new ProgressBar(document.querySelector("progress"));
-
-const dropZone = document.getElementById("dropZone");
 let filesToUpload = [];
 
-form.addEventListener("submit", handleSubmit);
-fileInput.addEventListener("change", handleInputChange);
-dropZone.addEventListener("drop", handleDrop);
-initDropZoneHighlightOnDrag();
+fileDropZone.enable().addDropHandle(handleDrop);
+form.addEventListener('submit', handleSubmit);
+fileInput.addEventListener('change', handleInputChange);
 
 function handleSubmit(event) {
   event.preventDefault();
   showPendingState();
-  // sendFiles(fileInput.files);
-  statusMessage.update("✅ Success");
-  const fileNames = Array.from(fileInput.files).map((file) => file.name);
-  fileUploads.render(fileNames);
+  sendFiles(fileInput.files);
 }
 
 function assertFilesValid(fileList) {
-  const allowedTypes = ["application/pdf"];
+  const allowedTypes = ['application/pdf'];
 
   for (const file of fileList) {
     if (!allowedTypes.includes(file.type)) {
@@ -57,52 +52,11 @@ function handleInputChange() {
 function resetFormState() {
   submitButton.disabled = true;
   statusMessage.update(`🤷‍♂ Nothing's uploaded`);
-  fileCounter.textContent = "0";
 }
 
 function showPendingState() {
   submitButton.disabled = true;
-  statusMessage.update("⏳ Pending...");
-}
-
-function initDropZoneHighlightOnDrag() {
-  let dragEventCounter = 0;
-
-  dropZone.addEventListener("dragenter", (event) => {
-    event.preventDefault();
-
-    if (dragEventCounter === 0) {
-      dropZone.classList.add("highlight");
-    }
-
-    dragEventCounter += 1;
-  });
-
-  dropZone.addEventListener("dragover", (event) => {
-    event.preventDefault();
-
-    if (dragEventCounter === 0) {
-      dragEventCounter = 1;
-    }
-  });
-
-  dropZone.addEventListener("dragleave", (event) => {
-    event.preventDefault();
-
-    dragEventCounter -= 1;
-
-    if (dragEventCounter <= 0) {
-      dragEventCounter = 0;
-      dropZone.classList.remove("highlight");
-    }
-  });
-
-  dropZone.addEventListener("drop", (event) => {
-    event.preventDefault();
-
-    dragEventCounter = 0;
-    dropZone.classList.remove("highlight");
-  });
+  statusMessage.update('⏳ Pending...');
 }
 
 function handleDrop(event) {
@@ -119,27 +73,27 @@ function handleDrop(event) {
 }
 
 function sendFiles(files) {
-  const url = "https://httpbin.org/post";
-  const method = "POST";
+  const url = 'https://httpbin.org/post';
+  const method = 'POST';
   const xhr = new XMLHttpRequest();
 
   const data = new FormData();
   for (const file of files) {
-    data.append("file", file);
+    data.append('file', file);
   }
 
-  xhr.addEventListener("loadend", () => {
+  xhr.addEventListener('loadend', () => {
     if (xhr.status === 200) {
-      statusMessage.update("✅ Success");
+      statusMessage.update('✅ Success');
       const fileNames = Array.from(fileInput.files).map((file) => file.name);
       fileUploads.render(fileNames);
     } else {
-      statusMessage.update("❌ Error");
+      statusMessage.update('❌ Error');
     }
     progressBar.update(0);
   });
 
-  xhr.upload.addEventListener("progress", (event) => {
+  xhr.upload.addEventListener('progress', (event) => {
     statusMessage.update(`⏳ Uploaded ${event.loaded} bytes of ${event.total}`);
     progressBar.update(event.loaded / event.total);
   });
