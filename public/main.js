85654e4ce3b3331a5d3e8e688cbc769228fd3e15
Benjamin <128577990+bencoronard@users.noreply.github.com>
2024-05-05 09:32:54 +0700

    Added options

--- public/main.js
+++ public/main.js
@@ -6,54 +6,83 @@ import {
   DropZone,
   PreviewListInteractive,
   ChromaticList,
-} from "./modules/components.js";
+} from './modules/components.js';
 
 const filesToUpload = [];
 const filesToDownload = [];
 
 const downloadButton = new DynamicButton(
-  document.getElementById("downloadButton")
+  document.getElementById('downloadButton')
 );
-const resetButton = new DynamicButton(document.getElementById("resetButton"));
-const selectButton = new DynamicButton(document.getElementById("selectButton"));
-const submitButton = new DynamicButton(document.getElementById("submitButton"));
+const resetButton = new DynamicButton(document.getElementById('resetButton'));
+const selectButton = new DynamicButton(document.getElementById('selectButton'));
+const submitButton = new DynamicButton(document.getElementById('submitButton'));
 
 const fileDropZone = new DropZone(
-  document.getElementById("dropZone"),
+  document.getElementById('dropZone'),
   (event) => {
     handleFileUpload(event.dataTransfer.files);
   }
 );
 const filePreviews = new PreviewListInteractive(
-  document.getElementById("filePreviews"),
+  document.getElementById('filePreviews'),
   filesToUpload
 );
 
-const statusMessage = new StatusText(document.getElementById("statusMessage"));
-const progressBar = new ProgressBar(document.querySelector("progress"));
-const stageList = new ChromaticList(document.getElementById("stageList"));
+const statusMessage = new StatusText(document.getElementById('statusMessage'));
+const progressBar = new ProgressBar(document.querySelector('progress'));
+const stageList = new ChromaticList(document.getElementById('stageList'));
 
-const form = document.querySelector("form");
-const fileInput = document.getElementById("fileInput");
+const form = document.querySelector('form');
+const fileInput = document.getElementById('fileInput');
 
 selectButton.addClickHandle(() => {
   fileInput.click();
 });
 downloadButton.addClickHandle(handleDownload);
 
-form.addEventListener("submit", handleSubmit);
-form.addEventListener("reset", resetFormState);
-fileInput.addEventListener("change", () => {
+form.addEventListener('submit', handleSubmit);
+form.addEventListener('reset', resetFormState);
+fileInput.addEventListener('change', () => {
   handleFileUpload(fileInput.files);
 });
 
 resetFormState();
 
+const observer = new MutationObserver((mutationsList) => {
+  for (let mutation of mutationsList) {
+    if (mutation.type === 'childList') {
+      const a = filesToUpload.length > 0 ? 1 : 0;
+      const b = filesToDownload.length > 0 ? 1 : 0;
+      switch (`${a}${b}`) {
+        case '00':
+          submitButton.disable();
+          resetButton.disable();
+          stageList.setStage(0);
+          break;
+        case '10':
+          submitButton.enable();
+          resetButton.enable();
+          stageList.setStage(1);
+          break;
+        case '01':
+          selectButton.disable();
+          submitButton.disable();
+          downloadButton.enable();
+          fileDropZone.disable();
+          stageList.setStage(2);
+          break;
+      }
+    }
+  }
+});
+observer.observe(filePreviews.pointer, { childList: true });
+
 // Functions
 function resetFormState() {
   filesToUpload.splice(0);
   filesToDownload.splice(0);
-  fileInput.value = "";
+  fileInput.value = '';
 
   filePreviews.clear();
 
@@ -81,7 +110,7 @@ function handleFileUpload(uploadedFiles) {
 }
 
 function handleDownload() {
-  console.log("Downloading...");
+  console.log('Downloading...');
 }
 
 function handleSubmit(event) {
@@ -92,7 +121,7 @@ function handleSubmit(event) {
 }
 
 function extractFiles(fileList) {
-  const allowedTypes = ["application/pdf"];
+  const allowedTypes = ['application/pdf'];
   const existingFiles = filesToUpload.map((file) => file.name);
   const invalidFiles = [];
   for (const file of fileList) {
@@ -106,57 +135,57 @@ function extractFiles(fileList) {
   }
   if (invalidFiles.length) {
     throw new Error(
-      `❌ Could not upload following files: ${invalidFiles.join(", ")}`
+      `❌ Could not upload following files: ${invalidFiles.join(', ')}`
     );
   }
 }
 
 function mockSending() {
-  statusMessage.update("⏳ Pending...");
+  statusMessage.update('⏳ Pending...');
 
   setTimeout(() => {
     filesToUpload.forEach((file) => {
       filesToDownload.push(file);
     });
     filesToUpload.splice(0);
-    statusMessage.update("✅ Success");
+    statusMessage.update('✅ Success');
     progressBar.update(0);
     filePreviews.update(
-      filesToDownload.map((file) => file.name + " is ready."),
+      filesToDownload.map((file) => file.name + ' is ready.'),
       false
     );
   }, 2000);
 }
 
 function sendFiles(files) {
-  statusMessage.update("⏳ Pending...");
-  const url = "https://httpbin.org/post";
-  const method = "POST";
+  statusMessage.update('⏳ Pending...');
+  const url = 'https://httpbin.org/post';
+  const method = 'POST';
   const xhr = new XMLHttpRequest();
 
   const data = new FormData();
   for (const file of files) {
-    data.append("file", file);
+    data.append('file', file);
   }
 
-  xhr.addEventListener("loadend", () => {
+  xhr.addEventListener('loadend', () => {
     if (xhr.status === 200) {
       filesToUpload.forEach((file) => {
         filesToDownload.push(file);
       });
       filesToUpload.splice(0);
       filePreviews.update(
-        filesToDownload.map((file) => file.name + " is ready."),
+        filesToDownload.map((file) => file.name + ' is ready.'),
         false
       );
-      statusMessage.update("✅ Success");
+      statusMessage.update('✅ Success');
     } else {
-      statusMessage.update("❌ Error");
+      statusMessage.update('❌ Error');
     }
     progressBar.update(0);
   });
 
-  xhr.upload.addEventListener("progress", (event) => {
+  xhr.upload.addEventListener('progress', (event) => {
     statusMessage.update(`⏳ Uploaded ${event.loaded} bytes of ${event.total}`);
     progressBar.update(event.loaded / event.total);
   });
@@ -164,32 +193,3 @@ function sendFiles(files) {
   xhr.open(method, url);
   xhr.send(data);
 }
-
-const observer = new MutationObserver((mutationsList) => {
-  for (let mutation of mutationsList) {
-    if (mutation.type === "childList") {
-      const a = filesToUpload.length > 0 ? 1 : 0;
-      const b = filesToDownload.length > 0 ? 1 : 0;
-      switch (`${a}${b}`) {
-        case "00":
-          submitButton.disable();
-          resetButton.disable();
-          stageList.setStage(0);
-          break;
-        case "10":
-          submitButton.enable();
-          resetButton.enable();
-          stageList.setStage(1);
-          break;
-        case "01":
-          selectButton.disable();
-          submitButton.disable();
-          downloadButton.enable();
-          fileDropZone.disable();
-          stageList.setStage(2);
-          break;
-      }
-    }
-  }
-});
-observer.observe(filePreviews.pointer, { childList: true });
