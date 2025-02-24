4bdde0fe2edcdb4fea68246c28d8a9e6b16f68db
Ben <128577990+bencoronard@users.noreply.github.com>
2025-02-24 16:33:46 +0700

    Create SecurityConfig.java

---
+++ src/main/java/dev/hireben/demo/rest/web/bff/infrastructure/security/configuration/SecurityConfig.java
@@ -0,0 +1,37 @@
+package dev.hireben.demo.rest.web.bff.infrastructure.security.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import dev.hireben.demo.rest.web.bff.utility.EnvironmentUtil;
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+  // ---------------------------------------------------------------------------//
+  // Fields
+  // ---------------------------------------------------------------------------//
+
+  // ---------------------------------------------------------------------------//
+  // Dependencies
+  // ---------------------------------------------------------------------------//
+
+  private final EnvironmentUtil environment;
+
+  // ---------------------------------------------------------------------------//
+  // Methods
+  // ---------------------------------------------------------------------------//
+
+  @Bean
+  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    environment.isDev();
+    return http.build();
+  }
+
+  // ---------------------------------------------------------------------------//
+
+}
