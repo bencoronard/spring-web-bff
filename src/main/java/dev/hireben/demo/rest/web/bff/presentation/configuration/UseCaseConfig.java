af48622fed293d2ba37635e15b91fce39cd3e183
Ben <128577990+bencoronard@users.noreply.github.com>
2025-02-25 11:38:30 +0700

    Update UseCaseConfig.java

--- src/main/java/dev/hireben/demo/rest/web/bff/presentation/configuration/UseCaseConfig.java
+++ src/main/java/dev/hireben/demo/rest/web/bff/presentation/configuration/UseCaseConfig.java
@@ -1,12 +1,6 @@
 package dev.hireben.demo.rest.web.bff.presentation.configuration;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import dev.hireben.demo.rest.web.bff.application.service.AuthenticationService;
-import dev.hireben.demo.rest.web.bff.application.service.PermissionService;
-import dev.hireben.demo.rest.web.bff.application.usecase.AuthenticationUseCase;
-import dev.hireben.demo.rest.web.bff.application.usecase.AuthorizationUseCase;
 
 @Configuration
 public class UseCaseConfig {
@@ -15,16 +9,18 @@ public class UseCaseConfig {
   // Methods
   // ---------------------------------------------------------------------------//
 
-  @Bean
-  AuthenticationUseCase authenticationUseCase(AuthenticationService authenticationService) {
-    return new AuthenticationUseCase(authenticationService);
-  }
+  // @Bean
+  // AuthenticationUseCase authenticationUseCase(AuthenticationService
+  // authenticationService) {
+  // return new AuthenticationUseCase(authenticationService);
+  // }
 
   // ---------------------------------------------------------------------------//
 
-  @Bean
-  AuthorizationUseCase authorizationUseCase(PermissionService permissionService) {
-    return new AuthorizationUseCase(permissionService);
-  }
+  // @Bean
+  // AuthorizationUseCase authorizationUseCase(PermissionService
+  // permissionService) {
+  // return new AuthorizationUseCase(permissionService);
+  // }
 
 }
