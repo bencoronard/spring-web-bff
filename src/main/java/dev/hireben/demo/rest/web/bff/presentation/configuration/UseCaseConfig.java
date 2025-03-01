f5da1208169d816aff534a59c82a02c2fdd074e9
Ben <128577990+bencoronard@users.noreply.github.com>
2025-03-01 21:45:23 +0700

    Implement use case configurations

--- src/main/java/dev/hireben/demo/rest/web/bff/presentation/configuration/UseCaseConfig.java
+++ src/main/java/dev/hireben/demo/rest/web/bff/presentation/configuration/UseCaseConfig.java
@@ -1,6 +1,15 @@
 package dev.hireben.demo.rest.web.bff.presentation.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import dev.hireben.demo.rest.web.bff.application.service.AuthenticationService;
+import dev.hireben.demo.rest.web.bff.application.service.PermissionService;
+import dev.hireben.demo.rest.web.bff.application.service.SyncTokenService;
+import dev.hireben.demo.rest.web.bff.application.usecase.AuthenticationUseCase;
+import dev.hireben.demo.rest.web.bff.application.usecase.AuthorizationUseCase;
+import dev.hireben.demo.rest.web.bff.domain.repository.SezzionRepository;
 
 @Configuration
 public class UseCaseConfig {
@@ -9,18 +18,24 @@ public class UseCaseConfig {
   // Methods
   // ---------------------------------------------------------------------------//
 
-  // @Bean
-  // AuthenticationUseCase authenticationUseCase(AuthenticationService
-  // authenticationService) {
-  // return new AuthenticationUseCase(authenticationService);
-  // }
+  @Bean
+  AuthenticationUseCase authenticationUseCase(
+      SezzionRepository sessionRepository,
+      SyncTokenService syncTokenService,
+      AuthenticationService authenService,
+      @Value("${application.auth.session.max-per-user}") int maxActiveUserSessions,
+      @Value("${application.auth.session.user-ttl}") int userSessionTtl) {
+    return new AuthenticationUseCase(maxActiveUserSessions, 0, sessionRepository, authenService, syncTokenService);
+  }
 
   // ---------------------------------------------------------------------------//
 
-  // @Bean
-  // AuthorizationUseCase authorizationUseCase(PermissionService
-  // permissionService) {
-  // return new AuthorizationUseCase(permissionService);
-  // }
+  @Bean
+  AuthorizationUseCase authorizationUseCase(
+      SezzionRepository sessionRepository,
+      PermissionService permissionService,
+      @Value("${application.auth.session.anonymous-ttl}") int anonymousSessionTtl) {
+    return new AuthorizationUseCase(anonymousSessionTtl, sessionRepository, permissionService);
+  }
 
 }
