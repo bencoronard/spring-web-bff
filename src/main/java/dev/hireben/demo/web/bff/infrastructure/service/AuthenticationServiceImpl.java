f722ef8fe05aa6cdd16c988c9c3d1e4c5460c08e
Ben <128577990+bencoronard@users.noreply.github.com>
2025-03-04 11:11:18 +0700

    Add mock authentication users

--- src/main/java/dev/hireben/demo/web/bff/infrastructure/service/AuthenticationServiceImpl.java
+++ src/main/java/dev/hireben/demo/web/bff/infrastructure/service/AuthenticationServiceImpl.java
@@ -1,5 +1,7 @@
 package dev.hireben.demo.web.bff.infrastructure.service;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
@@ -14,6 +16,46 @@ public class AuthenticationServiceImpl implements AuthenticationService {
   // Fields
   // ---------------------------------------------------------------------------//
 
+  private final Collection<Uzer> MOCK_USERS = List.of(
+      Uzer.builder()
+          .id("1")
+          .firstName("Jira")
+          .lastName("555")
+          .badgeNum("tang-dev-555")
+          .position("Software Developer")
+          .roleId("DEV-1")
+          .tenant("CONSENT")
+          .build(),
+      Uzer.builder()
+          .id("2")
+          .firstName("Kevin")
+          .lastName("888")
+          .badgeNum("k-dev-888")
+          .position("Software Engineer")
+          .roleId("DEV-2")
+          .tenant("NOTIFICATION")
+          .build(),
+      Uzer.builder()
+          .id("3")
+          .firstName("MamMOSFET")
+          .lastName("666")
+          .badgeNum("mothz-dev-666")
+          .position("System Engineer")
+          .roleId("DEV-3")
+          .tenant("INSURANCE")
+          .build(),
+      Uzer.builder()
+          .id("4")
+          .firstName("Thirdzaza")
+          .lastName("111")
+          .badgeNum("third-dev-111")
+          .position("Database Administrator")
+          .roleId("DBA-1")
+          .tenant("AUCTION")
+          .build());
+
+  private final String MOCK_PASSWORD = "P@ssw0rd";
+
   // ---------------------------------------------------------------------------//
   // Dependencies
   // ---------------------------------------------------------------------------//
@@ -24,8 +66,10 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
   @Override
   public Optional<Uzer> authenticate(String username, String password) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'authenticate'");
+    return MOCK_USERS.stream()
+        .filter(user -> (user.getFirstName() + user.getLastName()).equalsIgnoreCase(username))
+        .filter(_ -> MOCK_PASSWORD.equals(password))
+        .findFirst();
   }
 
 }
--- src/main/java/dev/hireben/demo/web/bff/infrastructure/service/PermissionServiceImpl.java
+++ src/main/java/dev/hireben/demo/web/bff/infrastructure/service/PermissionServiceImpl.java
@@ -1,18 +1,33 @@
 package dev.hireben.demo.web.bff.infrastructure.service;
 
 import java.util.Collection;
+import java.util.Map;
 import java.util.Optional;
-
 import org.springframework.stereotype.Service;
 
 import dev.hireben.demo.web.bff.application.service.PermissionService;
 
 @Service
+@SuppressWarnings("unused")
 public class PermissionServiceImpl implements PermissionService {
 
   // ---------------------------------------------------------------------------//
   // Fields
   // ---------------------------------------------------------------------------//
+
+  private final Map<String, String> MOCK_VIEW_PERMISSIONS_MAP = Map.of(
+      "/consent", "CONSENT_OVERVIEW",
+      "/notification", "NOTIFICATION_OVERVIEW",
+      "/insurance", "INSURANCE_OVERVIEW",
+      "/auction", "AUCTION_OVERVIEW");
+  private final Map<String, String> MOCK_API_PERMISSIONS_MAP = Map.of(
+      "GET/resources", "RESOURCE_LIST",
+      "GET/resources/*", "RESOURCE_READ",
+      "POST/resources", "RESOURCE_CREATE",
+      "PATCH/resources/*", "RESOURCE_UPDATE",
+      "PUT/resources/*", "RESOURCE_REPLACE",
+      "DELETE/resources/*", "RESOURCE_DELETE");
+  private final Map<String, Object> MOCK_ROLE_PERMISSIONS_MAP = null;
 
   // ---------------------------------------------------------------------------//
   // Dependencies
